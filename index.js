9f87393b33a8c88cb83a5084981092a5deb5ca7b
aaivey22 <angelawings22@gmail.com>
2020-04-06 16:07:19 -0400

    added const questions and shortened propmt

--- index.js
+++ index.js
@@ -2,25 +2,56 @@ var inquirer = require("inquirer");
 var fs = require('fs');
 const axios = require("axios");
 
+const questions = [
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "What is the project title?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "Give a brief description of this project.",
+        name: "description"
+      }
+      // {
+      //     type: "input",
+      //     message: "Table of contents?",
+      //     name: "contents"
+      //   },
+      //   {
+      //     type: "input",
+      //     message: "What is the installation?",
+      //     name: "installation"
+      //   },
+      //   {
+      //     type: "input",
+      //     message: "How is this project licesned?",
+      //     name: "license"
+      //   },
+      //   {
+      //     type: "input",
+      //     message: "What is the project title?",
+      //     name: "title"
+      //   },
+      //   {
+      //     type: "list",
+      //     message: "List contributors to this project",
+      //     name: "contributors"
+      //   },
+      //   {
+      //     type: "input",
+      //     message: "Tests?",
+      //     name: "test"
+      //   },
+]
 
 inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your github username?",
-      name: "username"
-    },
-    {
-      type: "input",
-      message: "What is your favorite project?",
-      name: "favorite"
-    },
-    {
-      type: "input",
-      message: "Why is it your favorite?",
-      name: "information"
-    }
-  ])
+  .prompt(questions)
   .then(function(response) {
 
 //     if (response.confirm === response.password) {
