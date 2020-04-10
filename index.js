cd5cd0599428a4e9d1ac3574ffe6a0cfdc5852be
aaivey22 <angelawings22@gmail.com>
2020-04-10 00:40:12 -0400

    added writeFile function in index

--- index.js
+++ index.js
@@ -1,75 +1,69 @@
-var inquirer = require("inquirer");
-var fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require('fs');
 const axios = require("axios");
+const api = require("./utils/api")
 
 const questions = [
-    {
-        type: "input",
-        message: "What is your github username?",
-        name: "username"
-      },
-      {
-        type: "input",
-        message: "What is the project title?",
-        name: "title"
-      },
-      {
-        type: "input",
-        message: "Give a brief description of this project.",
-        name: "description"
-      }
-      // {
-      //     type: "input",
-      //     message: "Table of contents?",
-      //     name: "contents"
-      //   },
-      //   {
-      //     type: "input",
-      //     message: "What is the installation?",
-      //     name: "installation"
-      //   },
-      //   {
-      //     type: "input",
-      //     message: "How is this project licesned?",
-      //     name: "license"
-      //   },
-      //   {
-      //     type: "input",
-      //     message: "What is the project title?",
-      //     name: "title"
-      //   },
-      //   {
-      //     type: "list",
-      //     message: "List contributors to this project",
-      //     name: "contributors"
-      //   },
-      //   {
-      //     type: "input",
-      //     message: "Tests?",
-      //     name: "test"
-      //   },
+  {
+    type: "input",
+    message: "What is your github username?",
+    name: "username"
+  },
+  {
+    type: "input",
+    message: "What is the project title?",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Give a brief description of this project.",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What is the link to the screenshot?",
+    name: "imageSource"
+  },
+  {
+    type: "input",
+    message: "What is the command for installation?",
+    name: "installation"
+  },
+  {
+    type: "input",
+    message: "How is this project licensed?",
+    name: "license"
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email"
+  }
 ];
 
-inquirer
-  .prompt(questions)
-  .then(function writeToFile(fileName, data) {
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
 
-    function init() {
-
+    if (err) {
+      return console.log(err);
     }
-    
-    init();
-//     if (response.confirm === response.password) {
-//       console.log("Hello " + response.username);
-//       console.log("Success!");
-//     }
-//     else {
-//       console.log("You forgot your password already?!");
-//     }
-//   });
-
-//   axios
-//   .get("https://www.omdbapi.com/?t=The%20Matrix&apikey=trilogy")
-//   .then(function(res) {
-    console.log(res.data);
+  
+    console.log("Success!");
+  
   });
+}
+
+function init() {
+  inquirer
+    .prompt(questions)
+    .then(res => {
+      api.getUser(res.username)
+        .then(api => {
+          console.log(api)
+          // writeToFile to generate readMe
+        })
+    });
+}
+
+init();
+// 
---
+++ utils/api.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+
+const api = {
+  getUser(username) {
+
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    axios.get(queryUrl).then(function (res) {
+      console.log("response",res.data)
+      const profileImage = res.data.map(function (repo) {
+        return repo.name;
+      });
+      const repoNamesStr = repoNames.join("\n");
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+      // use axios to get ghub api data
+      // remember to catch for errors
+      console.log(data)
+  }
+};
+
+  module.exports = api;
+
+// const fs = require("fs");
+// const axios = require("axios");
+// const inquirer = require("inquirer");
+
+// inquirer
+//   // .prompt({
+//   //   message: "Enter your GitHub username:",
+//   //   name: "username"
+//   // })
+//   .then(function({ username }) {
+//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+//     axios.get(queryUrl).then(function(res) {
+//       const repoNames = res.data.map(function(repo) {
+//         return repo.name;
+//       });
+
+//       const repoNamesStr = repoNames.join("\n");
+
+//       fs.writeFile("repos.txt", repoNamesStr, function(err) {
+//         if (err) {
+//           throw err;
+//         }
+
+//         console.log(`Saved ${repoNames.length} repos`);
+//       });
+//     });
+//   });
